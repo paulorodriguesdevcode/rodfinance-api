83f1fdaff6a7f02d25eb131033c9c0114cdafc55
Paulo Rodrigues <P39210@parceirosedu.com.br>
2023-01-27 11:47:47 -0300

    feat: add datadog config

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,16 @@
+import "./tracer";
 import { NestFactory } from '@nestjs/core';
+import { NextFunction, Request } from 'express';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const app = await NestFactory.create(AppModule, { cors: true });
+
+  app.use( (req: Request, res: Response, next: NextFunction) => {
+    console.log(`Method ${req.method} on ${req.url}`)
+    next()
+  })
+
+  await app.listen(3333);
 }
 bootstrap();
